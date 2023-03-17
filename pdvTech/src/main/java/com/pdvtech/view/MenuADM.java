71d86c581f7ec4a99ad5ece85cd8fc33462ac98e
Leonardo Lima <104847717+sirbickle@users.noreply.github.com>
2023-03-17 20:18:51 -0300

    Adição das telas de Listagem de Funci e cadastro de funci
    criação das interfaces graficas para Listagem de funci e criação de funci para o Sistemas

--- pdvTech/src/main/java/com/pdvtech/view/MenuADM.java
+++ pdvTech/src/main/java/com/pdvtech/view/MenuADM.java
@@ -44,8 +44,18 @@ public class MenuADM extends javax.swing.JFrame {
         jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/estoque.png"))); // NOI18N
 
         jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/pedido.png"))); // NOI18N
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/gerir.png"))); // NOI18N
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel1.setText("BALANÇO");
@@ -133,9 +143,19 @@ public class MenuADM extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        new Login_old().setVisible(true);
+        new Login().setVisible(true);
         MenuADM.this.dispose();
     }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+       new ListarFunci().setVisible(true);
+       MenuADM.this.dispose();
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        new Pedido().setVisible(true);
+        MenuADM.this.dispose();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
--- pdvTech/src/main/java/com/pdvtech/view/MenuFunci.java
+++ pdvTech/src/main/java/com/pdvtech/view/MenuFunci.java
@@ -90,7 +90,7 @@ public class MenuFunci extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        new Login_old().setVisible(true);
+        new Login().setVisible(true);
         MenuFunci.this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
