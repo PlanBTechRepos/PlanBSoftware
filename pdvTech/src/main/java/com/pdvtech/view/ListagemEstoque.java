a76336d81bd5450436217d5e1172991f1c80eb4e
Leonardo Lima <104847717+sirbickle@users.noreply.github.com>
2023-03-27 17:51:37 -0300

    alteração  na tela de listagem de estoque
    na tela de listagem de estoque foi retirado a "quantidade de saida" e "data de saida"

--- pdvTech/src/main/java/com/pdvtech/view/ListagemEstoque.java
+++ pdvTech/src/main/java/com/pdvtech/view/ListagemEstoque.java
@@ -55,11 +55,11 @@ public class ListagemEstoque extends javax.swing.JFrame {
 
             },
             new String [] {
-                "Codigo", "Nome", "Data de Entrada", "Data de Saida", "Quantidade de Saida", "Vencimento", "Valor"
+                "Codigo", "Nome", "Quantidade", "Data de Entrada", "Vencimento", "Valor"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Float.class
+                java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Float.class
             };
 
             public Class getColumnClass(int columnIndex) {
