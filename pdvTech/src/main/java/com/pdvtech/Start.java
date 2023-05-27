d41b036396d62ac2a14d396188b2e51534ef4b36
Douglas Fernandes Machado <douglas.machado@luizalabs.com>
2023-05-27 17:51:31 -0300

    Voltando login a main

--- pdvTech/src/main/java/com/pdvtech/Start.java
+++ pdvTech/src/main/java/com/pdvtech/Start.java
@@ -1,13 +1,11 @@
 
 package com.pdvtech;
 
-import com.pdvtech.controller.FuncionarioController;
-import com.pdvtech.controller.UsuarioController;
-import com.pdvtech.view.use.Employees;
+import com.pdvtech.view.use.Login;
 
 public class Start {
 
     public static void main(String[] args) {    
-        Employees.main(args);
+        Login.main(args);
     }
 }
